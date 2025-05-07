c79d36559b13ad8206ee8951a422fee1e54525b5
Arthur Ernoul De La Provôté <arthur.edlp@outlook.fr>
2025-05-07 09:50:27 +0200

    agencement des modèles

--- Meilleur modèle/ADAM ETS.R
+++ Meilleur modèle/ADAM ETS.R
@@ -1,39 +1,39 @@
 # AAA ----
 
-fit_ADAM_ETS <- auto.adam(ts_avalanche_wet_2022, model = "AAA", lags = c(1, 12), select = TRUE)
-fit_ADAM_ETS
+fit_ADAM_ETS_AAA <- auto.adam(ts_avalanche_wet_2022, model = "AAA", lags = c(1, 12), select = TRUE)
+fit_ADAM_ETS_AAA
 
 
 
 
 # AAN ----
 
-fit_ADAM_ETS <- auto.adam(ts_avalanche_wet_2022, model = "AAN", lags = c(1, 12), select = TRUE)
-fit_ADAM_ETS
+fit_ADAM_ETS_AAN <- auto.adam(ts_avalanche_wet_2022, model = "AAN", lags = c(1, 12), select = TRUE)
+fit_ADAM_ETS_AAN
 
 
 
 
 # ANA ----
 
-fit_ADAM_ETS <- auto.adam(ts_avalanche_wet_2022, model = "ANA", lags = c(1, 12), select = TRUE)
-fit_ADAM_ETS
+fit_ADAM_ETS_ANA <- auto.adam(ts_avalanche_wet_2022, model = "ANA", lags = c(1, 12), select = TRUE)
+fit_ADAM_ETS_ANA
 
 
 
 
 # NAA ----
 
-fit_ADAM_ETS <- auto.adam(ts_avalanche_wet_2022, model = "NAA", lags = c(1, 12), select = TRUE)
-fit_ADAM_ETS
+fit_ADAM_ETS_NAA <- auto.adam(ts_avalanche_wet_2022, model = "NAA", lags = c(1, 12), select = TRUE)
+fit_ADAM_ETS_NAA
 
 
 
 
 # ANN ----
 
-fit_ADAM_ETS <- auto.adam(ts_avalanche_wet_2022, model = "ANN", lags = c(1, 12), select = TRUE)
-fit_ADAM_ETS
+fit_ADAM_ETS_ANN <- auto.adam(ts_avalanche_wet_2022, model = "ANN", lags = c(1, 12), select = TRUE)
+fit_ADAM_ETS_ANN
 
 
 
@@ -41,24 +41,24 @@ fit_ADAM_ETS
 
 # NAN ----
 
-fit_ADAM_ETS <- auto.adam(ts_avalanche_wet_2022, model = "NAN", lags = c(1, 12), select = TRUE)
-fit_ADAM_ETS
+fit_ADAM_ETS_NAN <- auto.adam(ts_avalanche_wet_2022, model = "NAN", lags = c(1, 12), select = TRUE)
+fit_ADAM_ETS_NAN
 
 
  
 
 # NNA ----
 
-fit_ADAM_ETS <- auto.adam(ts_avalanche_wet_2022, model = "NNA", lags = c(1, 12), select = TRUE)
-fit_ADAM_ETS
+fit_ADAM_ETS_NNA <- auto.adam(ts_avalanche_wet_2022, model = "NNA", lags = c(1, 12), select = TRUE)
+fit_ADAM_ETS_NNA
 
 
 
 
 # NNN ----
 
-fit_ADAM_ETS <- auto.adam(ts_avalanche_wet_2022, model = "NNN", lags = c(1, 12), select = TRUE)
-fit_ADAM_ETS
+fit_ADAM_ETS_NNN <- auto.adam(ts_avalanche_wet_2022, model = "NNN", lags = c(1, 12), select = TRUE)
+fit_ADAM_ETS_NNN
 
 
 
--- Meilleur modèle/AES.R
+++ Meilleur modèle/AES.R
@@ -1,40 +1,39 @@
 # AAA ----
 
-fit_ADAM_ETS <- auto.adam(ts_avalanche_wet_2022,model="AAA", lags=c(1,1,12),
+fit_AES_AAA <- auto.adam(ts_avalanche_wet_2022,model="AAA", lags=c(1,1,12),
                           orders=list(ar=c(3,3), i=(2), ma=c(3,3), select=TRUE))
-fit_ADAM_ETS
+fit_AES_AAA
 
 
 
 
 # AAN ----
 
-fit_ADAM_ETS <- auto.adam(ts_avalanche_wet_2022, model = "AAN", lags=c(1,1,12),orders=list(ar=c(3,3), i=(2), ma=c(3,3), select=TRUE))
-fit_ADAM_ETS
+fit_AES_AAN <- auto.adam(ts_avalanche_wet_2022, model = "AAN", lags=c(1,1,12),orders=list(ar=c(3,3), i=(2), ma=c(3,3), select=TRUE))
+fit_AES_AAN
 
 
 
 
 # ANA ----
 
-fit_ADAM_ETS <- auto.adam(ts_avalanche_wet_2022, model = "ANA", lags=c(1,1,12),orders=list(ar=c(3,3), i=(2), ma=c(3,3), select=TRUE))
-fit_ADAM_ETS
+fit_AES_ANA <- auto.adam(ts_avalanche_wet_2022, model = "ANA", lags=c(1,1,12),orders=list(ar=c(3,3), i=(2), ma=c(3,3), select=TRUE))
+fit_AES_ANA
 
 
 
 
 # NAA ----
 
-fit_ADAM_ETS <- auto.adam(ts_avalanche_wet_2022, model = "NAA", lags=c(1,1,12),orders=list(ar=c(3,3), i=(2), ma=c(3,3), select=TRUE))
-fit_ADAM_ETS
-
+fit_AES_NAA <- auto.adam(ts_avalanche_wet_2022, model = "NAA", lags=c(1,1,12),orders=list(ar=c(3,3), i=(2), ma=c(3,3), select=TRUE))
+fit_AES_NAA
 
 
 
 # ANN ----
 
-fit_ADAM_ETS <- auto.adam(ts_avalanche_wet_2022, model = "ANN", lags=c(1,1,12),orders=list(ar=c(3,3), i=(2), ma=c(3,3), select=TRUE))
-fit_ADAM_ETS
+fit_AES_ANN <- auto.adam(ts_avalanche_wet_2022, model = "ANN", lags=c(1,1,12),orders=list(ar=c(3,3), i=(2), ma=c(3,3), select=TRUE))
+fit_AES_ANN
 
 
 
@@ -42,24 +41,24 @@ fit_ADAM_ETS
 
 # NAN ----
 
-fit_ADAM_ETS <- auto.adam(ts_avalanche_wet_2022, model = "NAN", lags=c(1,1,12),orders=list(ar=c(3,3), i=(2), ma=c(3,3), select=TRUE))
-fit_ADAM_ETS
+fit_AES_NAN <- auto.adam(ts_avalanche_wet_2022, model = "NAN", lags=c(1,1,12),orders=list(ar=c(3,3), i=(2), ma=c(3,3), select=TRUE))
+fit_AES_NAN
 
 
 
 
 # NNA ----
 
-fit_ADAM_ETS <- auto.adam(ts_avalanche_wet_2022, model = "NNA", lags=c(1,1,12),orders=list(ar=c(3,3), i=(2), ma=c(3,3), select=TRUE))
-fit_ADAM_ETS
+fit_AES_NNA <- auto.adam(ts_avalanche_wet_2022, model = "NNA", lags=c(1,1,12),orders=list(ar=c(3,3), i=(2), ma=c(3,3), select=TRUE))
+fit_AES_NNA
 
 
 
 
 # NNN ----
 
-fit_ADAM_ETS <- auto.adam(ts_avalanche_wet_2022, model = "NNN", lags=c(1,1,12),orders=list(ar=c(3,3), i=(2), ma=c(3,3), select=TRUE))
-fit_ADAM_ETS
+fit_AES_NNN <- auto.adam(ts_avalanche_wet_2022, model = "NNN", lags=c(1,1,12),orders=list(ar=c(3,3), i=(2), ma=c(3,3), select=TRUE))
+fit_AES_NNN
 
 
 
